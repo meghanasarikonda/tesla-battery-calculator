0a6393bb7a5a351f304f0a786095a03b818a1828
meghanasarikonda <meghana94987@gmail.com>
2018-01-27 18:33:03 +0530

    app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header/Header'
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>Let's get started</h2>
+      <div className="App">
+        <Header />
       </div>
     );
   }
