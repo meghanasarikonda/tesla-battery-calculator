5152a4bf0a65200c29d10e1c1f2dfa86f8c2239e
meghanasarikonda <meghana94987@gmail.com>
2018-01-28 00:02:24 +0530

    teslabattery files

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TeslaBattery extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Range Per Charge</h1>
+      </form>
+    )
+  }
+}
+
+export default TeslaBattery;
