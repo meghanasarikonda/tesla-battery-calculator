137ccb0754d6d64a15cfb511e39aa40c8c77e4e3
meghanasarikonda <meghana94987@gmail.com>
2018-01-28 00:01:39 +0530

    import TeslaBattery container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <TeslaBattery />
       </div>
     );
   }
